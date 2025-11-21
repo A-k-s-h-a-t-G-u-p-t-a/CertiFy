d27a23a617c4a8e568ea2048e71bc1a1796e9273
Kanavpreet-Singh <kanavpreetsingh2005@gmail.com>
2025-11-21 09:00:29 +0530

    fixing syntax issues

--- src/components/NavbarContent.jsx
+++ src/components/NavbarContent.jsx
@@ -15,18 +15,10 @@ import {
   MobileNavToggle,
   MobileNavMenu,
 } from "@/components/ui/navbar";
-import { useState } from "react";
-import { client } from "../lib/client";
-import { ConnectButton } from "thirdweb/react";
+
 export function NavbarDemo() {
   const { data: session } = useSession() // get session
-  const { data: session } = useSession() // get session
   const navItems = [
-    { name: "Admin", link: "/admin" },
-    { name: "Organizations", link: "/organizations" },
-    { name: "Verifier", link: "/verifier" },
-    { name: "Certificate Playground", link: "/certificate-generator" },
-  ]
     { name: "Admin", link: "/admin" },
     { name: "Organizations", link: "/organizations" },
     { name: "Verifier", link: "/verifier" },
